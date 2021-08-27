ffba783caf4492b8930307d926bd5d4d718d5704
Will <willstruder@gmail.com>
2021-08-27 00:27:18 -0400

    updated seed

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Workout = require('../models/workout.js');
+const Workout = require('../modals/workout');
 
 router.post('/api/workouts', (req, res) => {
   Workout.create({})
--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = require('../modals/workout');
+const db = require('../modals/workout.js');
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/Workout', 
